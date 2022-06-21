feda808faee1daffe197ee8bbccb6a3c94820069
admin <admin@DESKTOP-UFRCN2K>
2022-06-20 18:37:24 -0700

    Code Changes done for Java Testing Project

---
+++ JavaTestingProject/src/com/koti/javaTesting/TestingProject.java
@@ -0,0 +1,11 @@
+package com.koti.javaTesting;
+
+public class TestingProject {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Now i am commiting the code to Git");
+
+	}
+
+}
